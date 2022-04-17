033ed4d8b0412c03a675d49e8ea368aa78d45520
Manuel Sousa <mlrcbsousa@gmail.com>
2022-04-17 18:54:15 +0200

    Complete ex01.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,14 +6,38 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 18:20:00 by msousa            #+#    #+#             */
-/*   Updated: 2022/04/17 18:39:58 by msousa           ###   ########.fr       */
+/*   Updated: 2022/04/17 18:53:25 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-int	main( void )
+uintptr_t	serialize(Data* ptr)
 {
-	LOG("Hello world!");
-	return 0;
+    return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data*	deserialize(uintptr_t raw)
+{
+    return reinterpret_cast<Data*>(raw);
+}
+
+int main(void)
+{
+    Data 		data = {'a', 3, 3.6f, 8.23};
+	uintptr_t 	raw;
+	Data* 		ptr;
+
+    raw = serialize(&data);
+    ptr = deserialize(raw);
+
+    LOG("c: " << ptr->c);
+    LOG("i: " << ptr->i);
+    LOG("f: " << ptr->f);
+    LOG("d: " << ptr->d);
+
+    LOG("&data : " << &data);
+    LOG("ptr   : " << ptr);
+
+    return (0);
 }
